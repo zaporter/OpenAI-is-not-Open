e88afe125199649d14e924e5ed05df141e095ab9
zaporter <opensource@zackporter.com>
2023-03-24 21:28:08 -0400

    Update periodically

--- content_script.js
+++ content_script.js
@@ -1,27 +1,36 @@
 function replaceTextOnPage() {
+
   const openaiRegex = /openai/gi;
   const open_aiRegex = /open[ ]ai/gi;
 
-  // Get all the text nodes on the page
-  const walker = document.createTreeWalker(
-    document.body,
-    NodeFilter.SHOW_TEXT,
-    null,
-    false
-  );
-
-  // Loop through each text node and replace the matching text
-  let node;
-  while ((node = walker.nextNode())) {
-    const text = node.nodeValue;
+  function replaceText(node) {
+    const text = node.textContent;
     const replacedText = text
       .replace(openaiRegex, "\"Open\"AI")
       .replace(open_aiRegex, "\"Open\"-AI");
     if (replacedText !== text) {
-      node.nodeValue = replacedText;
+      node.textContent = replacedText;
     }
   }
+
+  function scanPageForNewNodes() {
+    const walker = document.createTreeWalker(
+      document.body,
+      NodeFilter.SHOW_TEXT,
+      null,
+      false
+    );
+    let node;
+    while ((node = walker.nextNode())) {
+      replaceText(node);
+    }
+  }
+
+  // Replace text on existing nodes
+  scanPageForNewNodes();
+
+  // Periodically scan the page for new nodes and replace text
+  setInterval(scanPageForNewNodes, 1000);
 }
 
 replaceTextOnPage();
-
